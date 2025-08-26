fa15d95ae02ea6701d184b4d73c793c5fc4f866d
Dmitriy Sinitsyn <sinitsyn.d.i@gmail.com>
2025-08-26 20:03:36 +0300

    Fix review

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,7 +7,7 @@
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.params.provider.Arguments.arguments;
 //
-//public class CalculatorTest {
+something {
 //
 //
 //    @ParameterizedTest
