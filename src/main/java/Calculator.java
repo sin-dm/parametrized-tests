2f02fa2d1fc25dfde72659706c64b856521f26fc
Dmitriy Sinitsyn <sinitsyn.d.i@gmail.com>
2025-11-18 20:11:04 +0300

    commets and sum refactoring

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,8 @@ public class Calculator {
 
     public int sum(int x, int y) {
         x *= 2;
-        y += 10;
+        y += 12;
+        //This method sum x and y
         return x + y;
     }
 }
